91a41173e70dd8acb5ebe66718c4c93f53ea42b0
Fernando da Silva Rosa <fernandosvrosa@gmail.com>
2023-09-12 08:55:49 -0300

    configs

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,75 @@
+local plugins = {  
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "eslint-lsp",
+        "prettier",
+        "typescript-language-server",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      require "custom.configs.dap"
+      require("core.utils").load_mappings("dap")
+    end
+  },
+ {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      require("dapui").setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function (_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    event = "VeryLazy",
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function (_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function ()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+}
+return plugins
